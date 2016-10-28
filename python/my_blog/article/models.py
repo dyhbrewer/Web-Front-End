fa0ce350e0fab9f4576a0fa3eb4d66169ce94b9c
dyhbrewer <xdxuhehui@126.com>
2016-10-28 21:20:37 +0800

    python blog

---
+++ python/my_blog/article/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100) #博客题目
+    category = models.CharField(max_length = 50,blank = True) #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)
+    content = models.TextField(blank = True,null = True)
+
+    def __str__(self):
+    	return self.title
+
+    class Meta: #按时间下降排序
+    	ordering = ['-date_time']
+	
+		
